f9c16c19caeaddea21650f0a9522842702546aae
Seth Shelnutt <Shelnutt2@gmail.com>
2022-04-29 09:06:46 -0400

    Add patch for pandas dtype issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,4 +5,7 @@ set -x
 
 export TILEDB_CONDA_BUILD=1
 
+# Patch pandas comparison issue
+patch -p1 < $RECIPE_DIR/0001-Fix-comparison-error-for-pandas-dataframe-dtype.patch
+
 $PYTHON setup.py install --tiledb="$PREFIX" --single-version-externally-managed --record record.txt
